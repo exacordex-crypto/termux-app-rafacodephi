c5868066606bdaa3ddc50bffb10e7e4558b9426e
Rafael mreis <rafaelmeloreisnovo@gmail.com>
2026-06-08 07:38:57 -0300

    fix: restore TermuxApplication with fatal init guards

--- app/src/main/java/com/termux/app/TermuxApplication.java
+++ app/src/main/java/com/termux/app/TermuxApplication.java
@@ -8,6 +8,4 @@ import com.termux.shared.errors.Error;
 import com.termux.shared.logger.Logger;
 import com.termux.shared.termux.TermuxBootstrap;
 import com.termux.shared.termux.TermuxConstants;
-import com.termux.shared.termux.crash.TermuxCrashUtils;
-import com.termux.shared.termux.file.TermuxFileUtils;
-import
+import com.termux.shared.termux.cr
